6a734ecde7c85905ae6cc23174ee30855fdac497
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-08-10 10:08:08 +0300

    Done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,33 @@
 class Movie < ActiveRecord::Base
+    def self.find_all_movies_by_year(year)
+        Movie.where(year: year)
+    end
 
+    def self.last_element
+        Movie.last()
+    end
+
+    def self.get_all
+        Movie.all
+    end
+
+    def count
+        Movie.count
+    end
+
+    def find(id)
+        Movie.find(id)
+    end
+
+    def find_by(name)
+        Movie.find_by title: name
+    end
+
+    def where()
+        Movie.find_by title: name
+    end
+
+    def destroy
+        Movie.destroy
+    end
 end
